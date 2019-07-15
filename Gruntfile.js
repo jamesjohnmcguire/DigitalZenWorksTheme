9e4d93e758c5470ce7e3f340c5b10a7573bec21b
JamesMc <jamesjohnmcguire@gmail.com>
2019-07-15 19:25:03 +0900

    Added grunt-contrib-cssmin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,22 @@
 module.exports = function(grunt)
 {
+	grunt.initConfig(
+	{
+		cssmin:
+		{
+			options:
+			{
+			},
+			target:
+			{
+				files:
+				{
+					'style.min.css': ['style.css']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ["cssmin"]);	
 };
