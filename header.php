c6e27153f7a1fa7645ab323f82a81aedf2dfb3d4
JamesMc <jamesjohnmcguire@gmail.com>
2019-07-15 20:47:53 +0900

    Added components from boilerplate template.

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+$language = 'en';
+$title = 'Digital Zen';
+$description = 'New theme';
+
+?>
+<!doctype html>
+<html class="no-js" lang="<?php echo $language; ?>">
+
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo $description; ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/boilerplate.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <meta name="theme-color" content="aqua">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
