266fba75928b185b4f0905e0fe0644a89045afdd
JamesMc <jamesjohnmcguire@gmail.com>
2020-05-24 16:30:19 +0900

    Added comment_debug function.

--- SourceCode/functions.php
+++ SourceCode/functions.php
@@ -1,5 +1,32 @@
 <?php
 
+function comment_debug()
+{
+	$item = '';
+	$post_id = '';
+
+	if (!empty($_SERVER))
+	{
+		if (array_key_exists('REQUEST_URI', $_SERVER))
+		{
+			$item = $_SERVER['REQUEST_URI'];
+			$item  = trim($item, '/');
+		}
+	}
+
+	if (!empty($item))
+	{
+		$post = get_page_by_path($item);
+
+		if (!empty($post))
+		{
+			$post_id = $post->ID;
+		}
+	}
+
+	echo "\r\n<!--*****DEBUG: item: $item :: post: $post_id*****-->\r\n";
+}
+
 function get_css_file($component)
 {
 	$css_file = '';
