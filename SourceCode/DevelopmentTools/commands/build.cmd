5b90b6a0eccf7ee8f3f4b538e5d2a3545914f3df
JamesMc <jamesjohnmcguire@gmail.com>
2021-07-23 23:42:56 +0900

    Added build command. Ignore built zip file.

--- SourceCode/DevelopmentTools/commands/build.cmd
+++ SourceCode/DevelopmentTools/commands/build.cmd
@@ -1,9 +1,18 @@
 CD %~dp0
 CD ..\..
 
-CALL composer outdated
-CALL composer install
+IF "%1"=="complete" GOTO complete
+GOTO deploy
 
+:complete
+CALL composer install
+ECHO outdated packages:
+CALL composer outdated
+
+ECHO PHP code styles
+CALL vendor\bin\phpcs -sp --standard=ruleset.xml .
+
+:deploy
 IF EXIST vendor.bak\NUL RD /S /Q vendor.bak
 IF EXIST vendor.export\NUL RD /S /Q vendor.export
 
@@ -18,5 +27,16 @@ CD vendor
 DEL /S /Q *.md >NUL
 CD ..
 
+IF EXIST DigitalZenTheme.zip DEL /Q DigitalZenTheme.zip
+
+7z a DigitalZenTheme.zip . -xr!.editorconfig -xr!composer.* -xr!Gruntfile.* -xr!package.* -xr!package-lock.* -x!node_modules -x!vendor.bak
+
 REN vendor vendor.export
 REN vendor.bak vendor
+
+CALL grunt sass
+CALL grunt cssmin
+CALL grunt uglify
+REM CALL grunt watch
+
+REM CALL UnitTests.cmd
