9094bd4f927f665e33f9359ae28b29bd90000d0c
JamesMc <jamesjohnmcguire@gmail.com>
2021-07-23 22:39:41 +0900

    Added basic tasks to Gruntfile.js.

--- SourceCode/Gruntfile.js
+++ SourceCode/Gruntfile.js
@@ -1,23 +1,61 @@
-module.exports = function(grunt) {
+module.exports = function(grunt)
+{
+	grunt.initConfig(
+	{
+		pkg: grunt.file.readJSON('package.json'),
+		cssmin:
+		{
+			options:
+			{
+			},
+			target:
+			{
+				files:
+				{
+				}
+			}
+		},
+		sass:
+		{
+			dev:
+			{
+				files:
+				{
+				}
+			}
+		},
+		uglify:
+		{
+			options:
+			{
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build:
+			{
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			},
+			my_target:
+			{
+				files:
+				{
+				}
+			}
+		},
+		watch:
+		{
+			targets:
+			{
+				files: ['js/**/*.js', 'assets/css/**/*.scss'],
+				tasks: ['sass', 'cssmin', 'uglify'],
+			}
+		}
+	});
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+	grunt.registerTask('default', ["cssmin", "sass", "uglify", "watch"]);
 };
