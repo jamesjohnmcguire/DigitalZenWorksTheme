9cdd5a5d2feaa53ece542df67c76147e9b16466f
JamesMc <jamesjohnmcguire@gmail.com>
2021-08-24 17:06:43 +0900

    Added custom functionality from an older theme.

--- SourceCode/functions.php
+++ SourceCode/functions.php
@@ -28,6 +28,11 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Post customiz additions.
+ */
+require get_template_directory() . '/inc/post-support.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
