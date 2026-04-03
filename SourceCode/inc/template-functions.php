19f42f22ceab63146ff7a44a3122a0cf2a09e3a3
James John McGuire <jamesjohnmcguire@gmail.com>
2026-04-03 13:50:00 +0900

    Improve Code Based on Code Analysis Warnings

--- SourceCode/inc/template-functions.php
+++ SourceCode/inc/template-functions.php
@@ -11,18 +11,24 @@
 /**
  * Adds custom classes to the array of body classes.
  *
- * @param array $classes Classes for the body element.
- * @return array
+ * @param string[] $classes Classes for the body element.
+ * @return string[]
  */
 function digitalzen_body_classes( array $classes ) : array
 {
+	$is_singular = is_singular();
+
 	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
+	if ( false === $is_singular )
+	{
 		$classes[] = 'hfeed';
 	}
 
+	$is_active_sidebar = is_active_sidebar( 'sidebar-1' );
+
 	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	if ( false === $is_active_sidebar )
+	{
 		$classes[] = 'no-sidebar';
 	}
 
